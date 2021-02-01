fed56723a08471b385c782723606151d4242fead
Vladimir Ostapenko <ostapenkov74@gmail.com>
2021-02-01 18:53:20 +0500

    add index.html.erb page

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
    validates :price, numericality: {greater_than: 0, allow_nill: true}
-   validates :name,:description, presence: true
+   validates :name,:description, presence: true,uniqueness:  true
    
    after_initialize {p 'initialize'}
    after_create {p 'create'}
