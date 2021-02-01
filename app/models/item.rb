1556ffef76181826b3a7d2427f904f786a6d3956
Vladimir Ostapenko <ostapenkov74@gmail.com>
2021-02-01 16:27:30 +0500

    add callbacks to model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
    validates :price, numericality: {greater_than: 0, allow_nill: true}
    validates :name,:description, presence: true
    
-   
-    
+   after_initialize {p 'initialize'}
+   after_create {p 'create'}
+   after_destroy {p 'destroy'} 
 end
